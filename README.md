6a1339a6d13197c10cf5d1f05b9cb9cdcadd772e
ness <duranr@protonmail.com>
2023-05-16 23:50:32 -0400

    Script mostly complete, README.md needs to be updated further

--- README.md
+++ README.md
@@ -7,6 +7,16 @@ Heavily inspired by gmzorz's prorec script
 
 This does not work.
 
+Author Notes:
+
+-   The -crf value is set to 0 which normally means lossless, so it may be extremely slow depending on file specifics.
+-   The tune is specifically set for film, so the output will have higher deblocking than normal to keep things looking smooth
+-   The preset is set to veryslow which prioritizes conserving the bitrate.
+
+For more information on H.264 encoding, read:
+
+https://trac.ffmpeg.org/wiki/Encode/H.264
+
 TESTS
 
 -   2023-05-16
--- remux.bat
+++ remux.bat
@@ -7,7 +7,7 @@ setlocal enabledelayedexpansion
 pushd %~dp0
     REM Change working directory to current directory of .bat file
 
-REM if [%1]==[] goto scan
+if [%1]==[] goto scan
     REM If no file is dropped into the batch, scan directory instead
 
     REM Drag & Drop
@@ -15,13 +15,37 @@ set file=%1
 set name=%~n1
     REM Set variables to full path + file name
 
-    REM Check if new folder exists
+    REM Check if 'remuxed' folder exists, if not create it
 if not exist "remuxed" ( mkdir "remuxed" )
 
     REM Do Command
-ffmpeg -i !file! -ar 22050 -crf 18 -movflags +faststart -pix_fmt yuv420p -strict -2 -acodec copy -vcodec copy "./remuxed/!name!.mp4"
+ffmpeg -i !file! -ar 22050 -crf 0 -tune animation -preset veryslow -movflags +faststart -pix_fmt yuv420p -strict -2 -acodec copy -vcodec copy "./remuxed/!name!.mp4"
 
     REM Ending Command
 echo Finished.
 pause >nul
+exit
+
+:scan 
+    REM Function for scanning files
+echo .mkv files found:
+echo. 
+
+for %%i in ("*.mkv") do ( echo %%i )
+    REM List all mkv files in the current directory
+
+echo.
+
+for %%a in ("*.mkv") do (
+    set namescan=%%~na
+
+    REM Check if 'remuxed' folder exists, if not create it
+    if not exist "remuxed" ( mkdir "remuxed" )
+
+    REM Remux
+    ffmpeg -i "%%a" -ar 22050 -crf 0 -tune animation -preset veryslow -movflags +faststart -pix_fmt yuv420p -strict -2 -acodec copy -vcodec copy "./remuxed/!namescan!.mp4"
+)
+    REM Ending Command
+echo Finished.
+pause >nul 
 exit
